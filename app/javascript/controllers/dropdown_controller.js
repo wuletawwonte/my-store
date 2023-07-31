db420079069f75876abe0645bd6cabf75ffce511
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-07-31 16:08:15 +0300

    Add click listener to hide menu when use clicks somewhere else

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,7 +1,15 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["menu"];
+  static targets = ["menu", "toggle"];
+
+  connect() {
+    document.addEventListener('click', (event) => {
+      if (!this.menuTarget.contains(event.target) && !this.toggleTarget.contains(event.target)) {
+        this.menuTarget.classList.add('hidden');
+      }
+    });
+  }
 
   toggle() {
     // this.element.classList.toggle("hidden");
