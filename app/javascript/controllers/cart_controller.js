2f72310ef0965716eb23b54aac447ee2b1a1278f
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-01 17:01:34 +0300

    Create a custom js DOM event to call action form another controller

---
+++ app/javascript/controllers/cart_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "stimulus";
+
+export default class extends Controller {
+  hide() {
+    this.element.classList.add("hidden");
+  }
+
+  show() {
+    this.element.classList.remove("hidden");
+  }
+}
