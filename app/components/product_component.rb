74f0386500e885ca02164e3f6f69dbdb1ed7b83b
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-05 17:34:15 +0300

    Build the product view component

--- app/components/product_component.rb
+++ app/components/product_component.rb
@@ -1,7 +1,6 @@
-class ProductComponent < ViewComponent::Base
-  attr_reader :title
-
-  def initialize(title:, price:, description:, category:, image:)
-    @title = title
-  end
+class ProductComponent < ApplicationComponent
+  param :title
+  param :price
+  option :image_url
+  option :description, default: -> { 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at purus.' }
 end
